9fc3a09f5e04b04decb0ba64ae8a50947c08f1a2
Mohammed <mopatel@my.yorku.ca>
2026-01-26 12:24:02 -0500

    Initial commit

---
+++ src/main/java/com/university/project/legendsofswordandwand/LegendsOfSwordAndWandApplication.java
@@ -0,0 +1,13 @@
+package com.university.project.legendsofswordandwand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendsOfSwordAndWandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendsOfSwordAndWandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/project/legendsofswordandwand/LegendsOfSwordAndWandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.project.legendsofswordandwand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendsOfSwordAndWandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
