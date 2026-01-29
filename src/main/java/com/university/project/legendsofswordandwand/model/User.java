2c0f2bad050bede47bae8a539ecace87ccc52224
Mohammed <mopatel@my.yorku.ca>
2026-01-29 14:55:11 -0500

    User Setup Testing

--- src/main/java/com/university/project/legendsofswordandwand/model/User.java
+++ src/main/java/com/university/project/legendsofswordandwand/model/User.java
@@ -1,7 +1,10 @@
 package com.university.project.legendsofswordandwand.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
@@ -9,26 +12,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     private String username;
+    @Setter
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/university/project/legendsofswordandwand/service/UserService.java
+++ src/main/java/com/university/project/legendsofswordandwand/service/UserService.java
@@ -1,4 +1,11 @@
 package com.university.project.legendsofswordandwand.service;
 
+import com.university.project.legendsofswordandwand.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private UserRepository userRepository;
 }
