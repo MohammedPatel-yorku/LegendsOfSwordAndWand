d389075c03720909b76409d6f7bf4591c0034b6e
Mohammed <mopatel@my.yorku.ca>
2026-02-07 21:21:31 -0500

    Test Fix

--- src/test/java/com/university/project/legendsofswordandwand/repository/UserRepositoryTest.java
+++ src/test/java/com/university/project/legendsofswordandwand/repository/UserRepositoryTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.university.project.legendsofswordandwand.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@DataJpaTest
 public class UserRepositoryTest {
 
   @Autowired private UserRepository userRepository;
