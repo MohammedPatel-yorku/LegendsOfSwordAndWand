d259b7d9d78b344a3c676e66dcdbf950ae0fd269
Mohammed <mopatel@my.yorku.ca>
2026-03-11 12:20:58 -0400

    Hero Class Creation

---
+++ src/main/java/com/university/project/legendsofswordandwand/battle/impl/ChaosBonusStrategy.java
@@ -0,0 +1,6 @@
+package com.university.project.legendsofswordandwand.battle.impl;
+
+import com.university.project.legendsofswordandwand.battle.ClassBonusStrategy;
+
+public class ChaosBonusStrategy implements ClassBonusStrategy {
+}
---
+++ src/main/java/com/university/project/legendsofswordandwand/battle/impl/MageBonusStrategy.java
@@ -0,0 +1,6 @@
+package com.university.project.legendsofswordandwand.battle.impl;
+
+import com.university.project.legendsofswordandwand.battle.ClassBonusStrategy;
+
+public class MageBonusStrategy implements ClassBonusStrategy {
+}
